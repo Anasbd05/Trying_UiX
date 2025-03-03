937d7fe2239f5d894bcb0405b9ef8598cd3e2eb3
anas bd <anastrying05@gmail.com>
2025-03-03 17:46:21 +0000

    user image

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-        hostname: "blob.haxed.net"
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "blob.haxed.net",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,28 @@
+"use client"
+import {supabase} from '@/lib/supabase'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 
 const Navbar = () => {
+
+    const [UserData,setUserData] = useState('')
+
+    const getUser = async () => {
+        const {data,error} = await supabase.auth.getSession()
+        if(error) {
+            console.log(error)
+        }
+        if(data.session) {
+            setUserData(data.session?.user.user_metadata)
+        }
+        console.log(UserData)
+    }
+
+    useEffect(() => {
+        getUser()
+    },[])
+
     return (
         <nav className='flex justify-between shadow-md  bg-white items-center px-10 mx-14 rounded-full py-2 '>
             <Image
@@ -21,9 +41,11 @@ const Navbar = () => {
                 <Link className='font-Ibm-sans duration-300  hover:py-1 hover:px-3 hover:bg-gray-200 rounded-lg ' href={"#pricing"}>Pricing</Link>
                 <Link className='font-Ibm-sans  duration-300 hover:py-1 hover:px-3 hover:bg-gray-200 rounded-lg ' href={"#faqs"}>Faqs</Link>
             </ul>
-            <Link href={"/login"}>
-                <button className='py-1 duration-500  bg-accent hover:shadow-[0px_3px_#000] px-5 rounded-full'>Sign In</button>
-            </Link>
+            {UserData !== "" ? <Image className='rounded-full p-1 bg-accent' src={UserData.avatar_url} alt='' height={40} width={40} /> :
+                <Link href={"/login"}>
+                    <button className='py-1 duration-500  bg-accent hover:shadow-[0px_3px_#000] px-5 rounded-full'>Sign In</button>
+                </Link>
+            }
         </nav>
     )
 }
